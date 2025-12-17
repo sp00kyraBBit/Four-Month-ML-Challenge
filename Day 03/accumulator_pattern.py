75e7c88d5bfca14582498bc17caeb6302430b7cb
Shahriar <shahriar21337@gmail.com>
2025-12-17 10:36:53 +0600

    bug fixed

--- Day 03/accumulator_pattern.py
+++ Day 03/accumulator_pattern.py
@@ -1,11 +1,12 @@
 raw_val = input("Enter a number: ")
 try:
     val = int(raw_val)
+    total = 0
+    for i in range(val+1):
+        total += i
+
+    print("The summation is ",total)
 except ValueError:
     print("Enter a valid number!!!")
 
-total = 0
-for i in range(val+1):
-    total += i
 
-print("The summation is ",total)
